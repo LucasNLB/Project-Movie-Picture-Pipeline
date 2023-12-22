de8e2295457fcf0749547f104b859a2564fe8e92
LucasNLTB <buit.bgvmm@gmail.com>
2023-12-23 01:39:50 +0700

    submit terraform and screenshot evidence

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -275,7 +275,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/MinhQuan-CTK35/Udacity_Project4"
+    location        = "https://github.com/LucasNLB/Project-Movie-Picture-Pipeline"
     git_clone_depth = 1
     buildspec       = "buildspec.yml"
   }
